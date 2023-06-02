5e16f6cba95b398c8cf0191fe65acc44ba1cba3e
seregaSkrrr <128396175+seregaSkrrr@users.noreply.github.com>
2023-06-02 12:26:06 +0300

    Described

--- ISave.h
+++ ISave.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "IDataModel.h"
 class ISave{
 	virtual void SaveToFile(const string fileName = "default.save");		
 	virtual void LoadFromFile(const string fileName = "default.save");		//виртуальные функции
