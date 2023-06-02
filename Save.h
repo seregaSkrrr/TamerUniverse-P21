99342071f27763f9a37e6450c513a0801b7b4a7c
seregaSkrrr <128396175+seregaSkrrr@users.noreply.github.com>
2023-06-02 12:27:34 +0300

    Described

--- Save.h
+++ Save.h
@@ -3,5 +3,7 @@
 class Save : public ISave{
 	void Save::SaveToFile(IDataModel dataModel) {
 
-}
-	void LoadFile();			//функция будет запрашивать переменные соответствующие составу игры: корабли, планеты и т.д. Будет их записывать в текстовый файл (приложенный в папке с этим проектом) и менять данные переданные в функцию.
+	}
+	void Save::LoadFromFile(IDataModel dataModel) {
+
+	}
