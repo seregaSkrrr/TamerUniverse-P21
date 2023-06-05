16fcf902f45bdb7d883b0920c8b342bfe4cca30a
seregaSkrrr <128396175+seregaSkrrr@users.noreply.github.com>
2023-06-05 18:00:04 +0300

    Skelet

--- ISave.h
+++ ISave.h
@@ -1,6 +1,6 @@
 #pragma once
-class ISave{
+class ISave {
 public:
-	virtual void SaveToFile()=0;
-	virtual void LoadFromFile()=0;
+	virtual void SaveToFile()=0;		//записывает данные из класса IDataModel в xml файл 
+	virtual void LoadFromFile()=0;		//считывает данные из xml файла
 };
