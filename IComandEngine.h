fb262070e9d7ccc6ee7be8f4803f5e3b35a52bcc
seregaSkrrr <128396175+seregaSkrrr@users.noreply.github.com>
2023-06-05 18:21:06 +0300

    Com andEngine skelet

--- IComandEngine.h
+++ IComandEngine.h
@@ -1,4 +1,12 @@
 #pragma once
+
 class IComandEngine {
+public:
+
+	virtual void FinishTurn();
+	virtual void ShowUniverseStatus();
+	virtual void SendFleet();
+	virtual void BuildShip();
+	virtual void ComandEng();
 
 };
