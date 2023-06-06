cc5c85faa2293770ada2da4c1f13da003d55bccc
seregaSkrrr <128396175+seregaSkrrr@users.noreply.github.com>
2023-06-06 13:35:40 +0300

    ara

--- ISave.h
+++ ISave.h
@@ -1,6 +1,6 @@
 #pragma once
 class ISave {
 public:
-	virtual void SaveToFile()=0;		//записывает данные из класса IDataModel в xml файл 
-	virtual void LoadFromFile()=0;		//считывает данные из xml файла
+	virtual void SaveToFile(const string FileName = "default.save")=0;		//записывает данные из класса IDataModel в xml файл 
+	virtual void LoadFromFile(const string FileName = "default.save")=0;		//считывает данные из xml файла
 };
