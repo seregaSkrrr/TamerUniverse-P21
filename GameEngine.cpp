c87eaa5e4bd561f307a1b7301724522ee11c21d0
Mggjd <128396846+Mggjd@users.noreply.github.com>
2023-06-07 16:59:28 +0300

    ara
    ara

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -1,1 +1,8 @@
 #include "GameEngine.h"
+#pragma once
+#include "IGameEngine.h"
+	void GameEngine::SaveMenu();
+	void GameEngine::StatusEmpire();
+	void GameEngine::EnteringCommands();
+	void GameEngine::OrganizationBattle();
+	void GameEngine::GameStartGame();
