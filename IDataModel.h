9afa5db6308491fb65463d59b3ab03cf622fff9d
AWPMasterGames <126659812+AWPMasterGames@users.noreply.github.com>
2023-06-07 17:00:20 +0300

    Update IDataModel.h
    Co-authored-by: AndreyChulov <128055929+AndreyChulov@users.noreply.github.com>

--- IDataModel.h
+++ IDataModel.h
@@ -2,7 +2,7 @@
 class IDataModel
 {
 public:
-	virtual void SendShip(int planetIndex);
+	virtual void SendFleet(int planetIndex);
 	virtual void BuildShip(int shield, int attack);
 	virtual void StartTurn();
 	virtual void EndTruen();
