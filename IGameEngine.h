0c368a8b33276b528a517bdcbac9f136b2204fdd
Mggjd <128396846+Mggjd@users.noreply.github.com>
2023-06-07 16:50:17 +0300

    ara

--- IGameEngine.h
+++ IGameEngine.h
@@ -1,10 +1,10 @@
 
 class IGameEngine
 {
-public:
-	virtual void savemenu();
-	virtual void statusEmpire();
-	virtual void enteringcommands();
-	virtual void organizationbattle();
-
+private:
+	virtual void SaveMenu();
+	virtual void StatusEmpire();
+	virtual void EnteringCommands();
+	virtual void OrganizationBattle();
+	virtual void StartGame();
 };
