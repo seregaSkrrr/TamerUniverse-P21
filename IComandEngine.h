a6994e4e1763b33357bba8e548e0447cb6684bc2
Xantrall <119814263+Xantrall@users.noreply.github.com>
2023-06-07 15:59:50 +0300

    Update IComandEngine.h

--- IComandEngine.h
+++ IComandEngine.h
@@ -1,4 +1,8 @@
 #pragma once
 class IComandEngine {
-
+virtual void FinishTurn();
+	virtual void ShowUniverseStatus() = 0;
+	virtual void SendFleet() = 0;
+	virtual void BuildShip() = 0;
+	virtual void ComandEng() = 0;
 };
