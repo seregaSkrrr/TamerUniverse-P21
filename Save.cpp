564cfb79e5076456138e949a47d5c22022a28251
seregaSkrrr <128396175+seregaSkrrr@users.noreply.github.com>
2023-06-08 15:22:20 +0300

    ara

--- Save.cpp
+++ Save.cpp
@@ -1,25 +1,4 @@
-//#include "Save.h"
 #pragma warning(disable : 4996)
-// 
-//void SaveLoadEngine::SaveToFile() {
-// 
-//}
-//
-//void SaveLoadEngine::LoadFromFile() {
-//	ofstream in;
-//	in.open("Universe.xml", ios::app);
-//	const char* Data = "Universe.xml";
-//	char buffer[100];
-//	FILE* FileManager = fopen(Data, "r+b");
-//	while ((fgets(buffer, 100, FileManager)) != NULL)
-//	{
-//		printf("%s", buffer);
-//	}
-//	fclose(FileManager);
-//}
-
-
-
 #include "SaveLoadEngine.h"
 
 void SaveLoadEngine::SaveToFile(){
