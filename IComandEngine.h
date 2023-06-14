43a9e27fa2ca9d2bf1865c57d353256e9bf9359b
Xantrall <119814263+Xantrall@users.noreply.github.com>
2023-06-14 17:50:42 +0300

    Update and rename IComandEngine.h to IComandEngine.h
    ara

--- IComandEngine.h
+++ IComandEngine.h
@@ -1,8 +1,9 @@
 #pragma once
 class IComandEngine {
-virtual void FinishTurn();
+public:
+        virtual void FinishTurn() = 0;
 	virtual void ShowUniverseStatus() = 0;
 	virtual void SendFleet() = 0;
-	virtual void BuildShip() = 0;
+	virtual void BuildShip(int shield,int attack) = 0;
 	virtual void ComandEng() = 0;
 };
