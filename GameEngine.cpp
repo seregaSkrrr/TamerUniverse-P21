509f9350cde83e4e009f496aae95f42fb1e1e149
Mggjd <128396846+Mggjd@users.noreply.github.com>
2023-06-14 17:02:01 +0300

    ага

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -2,7 +2,6 @@
 #pragma once
 void GameEngine::SaveMenu() {}
 void GameEngine::StatusEmpire() {}
-void GameEngine::CommandEnter() {}
+void GameEngine::EnterSaveCommand() {}
 void GameEngine::StartBattle() {}
-void GameEngine::GameStartGame() {}
 void GameEngine::StartGame() {}
