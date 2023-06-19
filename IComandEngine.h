86036489c6fbec8ac81a0d42df0a5a685eb307eb
Xantrall <119814263+Xantrall@users.noreply.github.com>
2023-06-19 16:41:40 +0300

    Update IComandEngine.h

--- IComandEngine.h
+++ IComandEngine.h
@@ -1,8 +1,11 @@
 #pragma once
 class IComandEngine {
+private:
+  virtual void SendFleet(int targetPlanetId) = 0;
+  virtual void BuildShip(int attack,int shield) = 0;
+  virtual void ShowUniverseStatus() = 0;
+  virtual void FinishTurn() = 0;
 public:
-        virtual void FinishTurn() = 0;
-	virtual void ShowUniverseStatus() = 0;
-	virtual void SendFleet() = 0;
-	virtual void BuildShip() = 0;
+  virtual void WelcomeMessage() = 0;
+  virtual void ShowHelp() = 0;
 };
