fbe22e3e01bd781c76e72fc738d944f9c3027643
Xantrall <119814263+Xantrall@users.noreply.github.com>
2023-06-19 16:21:31 +0300

    Update ComandEngine.h
    и\ваыи\аыц

--- ComandEngine.h
+++ ComandEngine.h
@@ -1,10 +1,10 @@
 #pragma once
 #include "IComandEngine.h"
 class ComandEngine :public IComandEngine{
-	private:
-		void FinishTurn();
-	void ShowUniverseStatus();
-public:
-void SendFleet(int planetID); 
-void BuildShip(int shield,int attack);
+  private:
+void FinishTurn();
+void ShowUniverseStatus();
+  public:
+void SendFleet(); 
+void BuildShip();
 };
