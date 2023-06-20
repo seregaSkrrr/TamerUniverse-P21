e4897e89f03eae0b4618efc6094dd9dd06d74602
Xantrall <119814263+Xantrall@users.noreply.github.com>
2023-06-20 22:39:20 +0300

    Update IComandEngine.h

--- IComandEngine.h
+++ IComandEngine.h
@@ -5,7 +5,6 @@ private:
     virtual void BuildShip(int attack,int shield) = 0;
     virtual void ShowUniverseStatus() = 0;
     virtual void FinishTurn() = 0;
-    virtual void StartEngine() = 0;
     virtual void WelcomeMessage() = 0;
     virtual void ShowHelp() = 0;
 public:
